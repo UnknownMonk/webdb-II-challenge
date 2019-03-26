906dd14b24ee981caf9a1c945146484cf49c7fa9
UnknownMonk <smodcartoon@gmail.com>
2019-03-26 16:07:04 -0400

    added routes and all dependencys

--- index.js
+++ index.js
@@ -1,14 +1,64 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
 
 const server = express();
-
+const db = knex(knexConfig.development);
 server.use(express.json());
+
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.send('Home Route Working');
+});
 
-const port = 3300;
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post('/api/zoos', (req, res) => {
+  db('zoos')
+    .insert(req.body)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoozs')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  const changes = req.body;
+  db('zoos')
+    .where({ id: req.params.id })
+    .update(changes)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+const port = 4000;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
